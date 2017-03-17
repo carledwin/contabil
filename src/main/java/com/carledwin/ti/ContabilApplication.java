09bf4809c61b6becc432b88e0eee8aeecdc4c7cc
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2017-03-17 15:36:15 -0300

    Inicial

---
+++ src/main/java/com/carledwin/ti/ContabilApplication.java
@@ -0,0 +1,12 @@
+package com.carledwin.ti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilApplication.class, args);
+	}
+}
